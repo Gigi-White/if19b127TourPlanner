b15314be1deed5abd483464d83c1493e4875c34e
Christian Weissenbacher <christian-weissenbacher@gmx.at>
2021-04-05 12:11:09 +0200

    Initial commit

---
+++ TourPlanner.Models/Tour.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TourPlanner.Models
+{
+    public class Tour
+    {
+        public string Name { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string CreationDate { get; set; }
+        public int Distance { get; set; }
+        public string image { get; set; }
+    }
+}
---
+++ TourPlanner/ViewModels/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using TourPlanner.Models;
+
+namespace TourPlanner.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+
+        
+
+        string searchTour;
+
+        public ObservableCollection <Tour> Tours { get; set; }
+
+        private Tour currentTour;
+
+        public Tour CurrentTour
+        {
+            get
+            {
+                return currentTour;
+            }
+            set
+            {
+                if(currentTour != value && value != null)
+                {
+                    currentTour = value;
+                    RaisePropertyChangedEvent(nameof(CurrentTour));
+                } 
+            }
+        }
+
+        public MainViewModel()
+        {
+            List<Tour> MyTours = new List<Tour>();
+
+            Tour eins = new Tour()
+            {
+                Name = "Gigi",
+                Start = "Wien",
+                End = "Berlin",
+                CreationDate = "26.04.2020",
+                Distance = 200,
+               
+            };
+
+            MyTours.Add(eins);
+
+
+            Tour zwei = new Tour()
+            {
+                Name = "Mike",
+                Start = "Hamburg",
+                End = "Berlin",
+                CreationDate = "25.04.2020",
+                Distance = 100,
+                
+            };
+
+            MyTours.Add(zwei);
+
+            Tours = new ObservableCollection<Tour>();
+            foreach (var item in MyTours)
+                Tours.Add(item);
+
+        }       
+
+    }
+
+}
